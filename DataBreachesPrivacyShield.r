36c072e6830becc349fed6f57f0b4a43d382909a
Jordi Vilana Tort <CVT@costaisa.org>
2017-06-09 18:59:15 +0200

    Commit v3

--- DataBreachesPrivacyShield.r
+++ DataBreachesPrivacyShield.r
@@ -12,9 +12,10 @@ PRTotal <- PRTotal[which(!duplicated(PRTotal$Company)),]
 # Busquedas con varias distancias, la maxdist=2 da unos resultados aceptables, ya tiene una tasa de error apreciable,
 # pero tambien tiene una tasa de exito a tener en cuenta.
 #IndsPRAmatchD1 <-  Inds[which(!is.na(amatch(Inds$CompanyName,PRTotal$Company,maxDist=1))),]
-IndsPRAmatchD2 <-  Inds[which(!is.na(amatch(Inds$CompanyName,PRTotal$Company,maxDist=2))),]
+IndsPRAmatchD2 <-  PRTotal[which(!is.na(amatch(PRTotal$Company,Inds$CompanyName,maxDist=2))),]
 #IndsPRAmatchD3 <-  Inds[which(!is.na(amatch(Inds$CompanyName,PRTotal$Company,maxDist=3))),]
 #IndsPRAmatchD4 <-  Inds[which(!is.na(amatch(Inds$CompanyName,PRTotal$Company,maxDist=4))),]
+
 saveRDS(IndsPRAmatchD2,file="./data/DataBreachesPrivacyShield.rds")
 
 
